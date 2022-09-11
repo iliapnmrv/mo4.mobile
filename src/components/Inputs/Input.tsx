bad53ec9a8eca4deeba2ff3e08b06d2461b0d37b
iliapnmrv <iliapnmrvv@gmail.com>
2022-09-11 23:30:36 +0300

    search input, refetch docs

--- src/components/Inputs/Input.tsx
+++ src/components/Inputs/Input.tsx
@@ -5,7 +5,9 @@ import {
   TextInput,
   View,
 } from 'react-native';
-import React, {Dispatch, SetStateAction} from 'react';
+import React, {Dispatch, ReactNode, SetStateAction} from 'react';
+import { COLORS } from 'constants/colors';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 type Props = {
   setValue: Dispatch<SetStateAction<string>>;
@@ -13,6 +15,7 @@ type Props = {
   placeholder?: string;
   keyboardType?: KeyboardTypeOptions;
   label?: string;
+  iconName?: string
 };
 
 const Input = ({
@@ -21,19 +24,23 @@ const Input = ({
   placeholder,
   label,
   keyboardType = 'default',
+  iconName
 }: Props) => {
   return (
     <>
       <Text style={styles.labelText}>{label}</Text>
-      <TextInput
-        onChangeText={setValue}
-        value={value}
-        placeholder={placeholder}
-        keyboardType={keyboardType}
-        style={styles.inputStyle}
-        placeholderTextColor="#dbdbdb"
-        selectionColor="#757575"
-      />
+      <View style={iconName ? styles.iconContainer : {}}>
+        {iconName ? <Icon style={styles.icon} name={iconName} size={20} color="#000"/> : null}
+        <TextInput
+          onChangeText={setValue}
+          value={value}
+          placeholder={placeholder}
+          keyboardType={keyboardType}
+          style={styles.inputStyle}
+          placeholderTextColor="#dbdbdb"
+          selectionColor="#757575"
+        />
+      </View>
     </>
   );
 };
@@ -43,16 +50,27 @@ export default Input;
 const styles = StyleSheet.create({
   inputStyle: {
     color: '#000',
-    fontSize: 22,
-    borderBottomWidth: 1,
+    fontSize: 18,
+    // borderBottomWidth: 1,
     borderColor: '#808080',
     borderRadius: 5,
-    paddingHorizontal: 20,
+    paddingHorizontal: 10,
     backgroundColor: '#fff',
-    marginBottom: 10,
+    // backgroundColor: COLORS.lightgray,
   },
   labelText: {
     color: '#808080',
     fontSize: 18,
+  },
+  iconContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+    marginBottom: 10,
+    borderLeftColor: 'black',
+    borderLeftWidth: 2
+  },
+  icon: {
+    paddingLeft: 15,
   },
 });
--- src/constants/colors.ts
+++ src/constants/colors.ts
@@ -1,4 +1,5 @@
 export const COLORS = {
     primary: '#000',
-    gray: '#808080'
+    gray: '#808080',
+    lightgray: '#ededed'
 }
